019e63e98740f532bea363fe84fab2fc43c7e4d2
Andre <99513007+AndreTsoi@users.noreply.github.com>
2026-09-15 16:35:44 -0400

    Delete CSS styles for volleyball video
    Removed CSS styles for volleyball video.

--- dist/volleyball-video.js
+++ dist/volleyball-video.js
@@ -9,7 +9,3 @@ if (volleyballVideo && VIDEO_URL) {
   volleyballVideo.autoplay = !reducedMotion.matches;
   if (!reducedMotion.matches) volleyballVideo.play().catch(() => {});
 }
-
-.volleyball-video{
-  filter: grayscale(1) contrast(1.35) brightness(1.05);
-}
